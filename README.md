51e9964d8296fbbe06b6cbd64010d8a41944fc3f
Daanchaam <daanhendriks97@gmail.com>
2021-06-12 09:58:02 +0200

    Started boundaries

---
+++ README.md
@@ -0,0 +1,17 @@
+# SiCG-Part-1
+
+Simulation in Computer Graphics Project Part 1
+
+## Running
+
+### Makefile (Mac, VS Code)
+
+- Make sure to `brew install libpng` and install C/C++ and MakeFile Tools packages
+- Run using the makefile extension
+
+### CMake (Linux, VS Code)
+
+- Make sure to install CMake, C/C++, libpng and freeglut
+- Then run `cmake -B build`
+- Next `cmake --build build`
+- And lastly, to run, `./build/2imv15_project_1`
--- src/solver.cpp
+++ src/solver.cpp
@@ -1,99 +1,157 @@
-#define IX(i,j) ((i)+(N+2)*(j))
-#define SWAP(x0,x) {float * tmp=x0;x0=x;x=tmp;}
-#define FOR_EACH_CELL for ( i=1 ; i<=N ; i++ ) { for ( j=1 ; j<=N ; j++ ) {
-#define END_FOR }}
-
-void add_source ( int N, float * x, float * s, float dt )
-{
-	int i, size=(N+2)*(N+2);
-	for ( i=0 ; i<size ; i++ ) x[i] += dt*s[i];
-}
-
-void set_bnd ( int N, int b, float * x )
-{
-	int i;
-
-	for ( i=1 ; i<=N ; i++ ) {
-		x[IX(0  ,i)] = b==1 ? -x[IX(1,i)] : x[IX(1,i)];
-		x[IX(N+1,i)] = b==1 ? -x[IX(N,i)] : x[IX(N,i)];
-		x[IX(i,0  )] = b==2 ? -x[IX(i,1)] : x[IX(i,1)];
-		x[IX(i,N+1)] = b==2 ? -x[IX(i,N)] : x[IX(i,N)];
+#define IX(i, j) ((i) + (N + 2) * (j))
+#define SWAP(x0, x)      \
+	{                    \
+		float *tmp = x0; \
+		x0 = x;          \
+		x = tmp;         \
 	}
-	x[IX(0  ,0  )] = 0.5f*(x[IX(1,0  )]+x[IX(0  ,1)]);
-	x[IX(0  ,N+1)] = 0.5f*(x[IX(1,N+1)]+x[IX(0  ,N)]);
-	x[IX(N+1,0  )] = 0.5f*(x[IX(N,0  )]+x[IX(N+1,1)]);
-	x[IX(N+1,N+1)] = 0.5f*(x[IX(N,N+1)]+x[IX(N+1,N)]);
+#define FOR_EACH_CELL            \
+	for (i = 1; i <= N; i++)     \
+	{                            \
+		for (j = 1; j <= N; j++) \
+		{
+#define END_FOR \
+	}           \
+	}
+
+void add_source(int N, float *x, float *s, float dt)
+{
+	int i, size = (N + 2) * (N + 2);
+	for (i = 0; i < size; i++)
+		x[i] += dt * s[i];
 }
 
-void lin_solve ( int N, int b, float * x, float * x0, float a, float c )
+/**
+ * @param (int) N: number of blocks in the grid
+ * @param (int) b: in {1,2} => (1 -> boundary on the right, 2-> boundary on the left)
+ * @param (float) x: place on the grid (counting from top left to bottom right)
+ * @param (float) boundaries: place on the grid where boundaries should be
+ */
+void set_bnd(int N, int b, float *x, float *boundaries)
+{
+	int i, j;
+
+	for (i = 1; i <= N; i++)
+	{
+		x[IX(0, i)] = b == 1 ? -x[IX(1, i)] : x[IX(1, i)];
+		x[IX(N + 1, i)] = b == 1 ? -x[IX(N, i)] : x[IX(N, i)];
+		x[IX(i, 0)] = b == 2 ? -x[IX(i, 1)] : x[IX(i, 1)];
+		x[IX(i, N + 1)] = b == 2 ? -x[IX(i, N)] : x[IX(i, N)];
+	}
+	// Create a boundary each block where this is defined
+	// Here we most likely will want to recompute after forces are exerted on boundaries
+	FOR_EACH_CELL
+	if (boundaries[IX(i, j)] == 1)
+	{
+		x[IX(i, j)] = 0; // The center has no impact on any fluids
+						 // TODO: The below comments should make sense but cause weird behaviour
+						 // // On the left and right, invert the x as happens above
+						 // x[IX(i - 1, j)] = b == 1 ? -x[IX(i - 2, j)] : x[IX(i - 2, j)];
+						 // x[IX(i + 1, j)] = b == 1 ? -x[IX(i + 2, j)] : x[IX(i + 2, j)];
+						 // // Also invert y vertically
+						 // x[IX(i, j - 1)] = b == 2 ? -x[IX(i, j - 2)] : x[IX(i, j - 2)];
+						 // x[IX(i, j + 1)] = b == 2 ? -x[IX(i, j + 2)] : x[IX(i, j + 2)];
+	}
+	END_FOR
+
+	x[IX(0, 0)] = 0.5f * (x[IX(1, 0)] + x[IX(0, 1)]);
+	x[IX(0, N + 1)] = 0.5f * (x[IX(1, N + 1)] + x[IX(0, N)]);
+	x[IX(N + 1, 0)] = 0.5f * (x[IX(N, 0)] + x[IX(N + 1, 1)]);
+	x[IX(N + 1, N + 1)] = 0.5f * (x[IX(N, N + 1)] + x[IX(N + 1, N)]);
+}
+
+void lin_solve(int N, int b, float *x, float *x0, float a, float c, float *boundaries)
 {
 	int i, j, k;
 
-	for ( k=0 ; k<20 ; k++ ) {
+	for (k = 0; k < 20; k++)
+	{
 		FOR_EACH_CELL
-			x[IX(i,j)] = (x0[IX(i,j)] + a*(x[IX(i-1,j)]+x[IX(i+1,j)]+x[IX(i,j-1)]+x[IX(i,j+1)]))/c;
+		x[IX(i, j)] = (x0[IX(i, j)] + a * (x[IX(i - 1, j)] + x[IX(i + 1, j)] + x[IX(i, j - 1)] + x[IX(i, j + 1)])) / c;
 		END_FOR
-		set_bnd ( N, b, x );
+		set_bnd(N, b, x, boundaries);
 	}
 }
 
-void diffuse ( int N, int b, float * x, float * x0, float diff, float dt )
+void diffuse(int N, int b, float *x, float *x0, float diff, float dt, float *boundaries)
 {
-	float a=dt*diff*N*N;
-	lin_solve ( N, b, x, x0, a, 1+4*a );
+	float a = dt * diff * N * N;
+	lin_solve(N, b, x, x0, a, 1 + 4 * a, boundaries);
 }
 
-void advect ( int N, int b, float * d, float * d0, float * u, float * v, float dt )
+void advect(int N, int b, float *d, float *d0, float *u, float *v, float dt, float *boundaries)
 {
 	int i, j, i0, j0, i1, j1;
 	float x, y, s0, t0, s1, t1, dt0;
 
-	dt0 = dt*N;
+	dt0 = dt * N;
 	FOR_EACH_CELL
-		x = i-dt0*u[IX(i,j)]; y = j-dt0*v[IX(i,j)];
-		if (x<0.5f) x=0.5f; if (x>N+0.5f) x=N+0.5f; i0=(int)x; i1=i0+1;
-		if (y<0.5f) y=0.5f; if (y>N+0.5f) y=N+0.5f; j0=(int)y; j1=j0+1;
-		s1 = x-i0; s0 = 1-s1; t1 = y-j0; t0 = 1-t1;
-		d[IX(i,j)] = s0*(t0*d0[IX(i0,j0)]+t1*d0[IX(i0,j1)])+
-					 s1*(t0*d0[IX(i1,j0)]+t1*d0[IX(i1,j1)]);
+	x = i - dt0 * u[IX(i, j)];
+	y = j - dt0 * v[IX(i, j)];
+	if (x < 0.5f)
+		x = 0.5f;
+	if (x > N + 0.5f)
+		x = N + 0.5f;
+	i0 = (int)x;
+	i1 = i0 + 1;
+	if (y < 0.5f)
+		y = 0.5f;
+	if (y > N + 0.5f)
+		y = N + 0.5f;
+	j0 = (int)y;
+	j1 = j0 + 1;
+	s1 = x - i0;
+	s0 = 1 - s1;
+	t1 = y - j0;
+	t0 = 1 - t1;
+	d[IX(i, j)] = s0 * (t0 * d0[IX(i0, j0)] + t1 * d0[IX(i0, j1)]) +
+				  s1 * (t0 * d0[IX(i1, j0)] + t1 * d0[IX(i1, j1)]);
 	END_FOR
-	set_bnd ( N, b, d );
+	set_bnd(N, b, d, boundaries);
 }
 
-void project ( int N, float * u, float * v, float * p, float * div )
+void project(int N, float *u, float *v, float *p, float *div, float *boundaries)
 {
 	int i, j;
 
 	FOR_EACH_CELL
-		div[IX(i,j)] = -0.5f*(u[IX(i+1,j)]-u[IX(i-1,j)]+v[IX(i,j+1)]-v[IX(i,j-1)])/N;
-		p[IX(i,j)] = 0;
-	END_FOR	
-	set_bnd ( N, 0, div ); set_bnd ( N, 0, p );
+	div[IX(i, j)] = -0.5f * (u[IX(i + 1, j)] - u[IX(i - 1, j)] + v[IX(i, j + 1)] - v[IX(i, j - 1)]) / N;
+	p[IX(i, j)] = 0;
+	END_FOR
+	set_bnd(N, 0, div, boundaries);
+	set_bnd(N, 0, p, boundaries);
 
-	lin_solve ( N, 0, p, div, 1, 4 );
+	lin_solve(N, 0, p, div, 1, 4, boundaries);
 
 	FOR_EACH_CELL
-		u[IX(i,j)] -= 0.5f*N*(p[IX(i+1,j)]-p[IX(i-1,j)]);
-		v[IX(i,j)] -= 0.5f*N*(p[IX(i,j+1)]-p[IX(i,j-1)]);
+	u[IX(i, j)] -= 0.5f * N * (p[IX(i + 1, j)] - p[IX(i - 1, j)]);
+	v[IX(i, j)] -= 0.5f * N * (p[IX(i, j + 1)] - p[IX(i, j - 1)]);
 	END_FOR
-	set_bnd ( N, 1, u ); set_bnd ( N, 2, v );
+	set_bnd(N, 1, u, boundaries);
+	set_bnd(N, 2, v, boundaries);
 }
 
-void dens_step ( int N, float * x, float * x0, float * u, float * v, float diff, float dt )
+void dens_step(int N, float *x, float *x0, float *u, float *v, float diff, float dt, float *boundaries)
 {
-	add_source ( N, x, x0, dt );
-	SWAP ( x0, x ); diffuse ( N, 0, x, x0, diff, dt );
-	SWAP ( x0, x ); advect ( N, 0, x, x0, u, v, dt );
+	add_source(N, x, x0, dt);
+	SWAP(x0, x);
+	diffuse(N, 0, x, x0, diff, dt, boundaries);
+	SWAP(x0, x);
+	advect(N, 0, x, x0, u, v, dt, boundaries);
 }
 
-void vel_step ( int N, float * u, float * v, float * u0, float * v0, float visc, float dt )
+void vel_step(int N, float *u, float *v, float *u0, float *v0, float visc, float dt, float *boundaries)
 {
-	add_source ( N, u, u0, dt ); add_source ( N, v, v0, dt );
-	SWAP ( u0, u ); diffuse ( N, 1, u, u0, visc, dt );
-	SWAP ( v0, v ); diffuse ( N, 2, v, v0, visc, dt );
-	project ( N, u, v, u0, v0 );
-	SWAP ( u0, u ); SWAP ( v0, v );
-	advect ( N, 1, u, u0, u0, v0, dt ); advect ( N, 2, v, v0, u0, v0, dt );
-	project ( N, u, v, u0, v0 );
+	add_source(N, u, u0, dt);
+	add_source(N, v, v0, dt);
+	SWAP(u0, u);
+	diffuse(N, 1, u, u0, visc, dt, boundaries);
+	SWAP(v0, v);
+	diffuse(N, 2, v, v0, visc, dt, boundaries);
+	project(N, u, v, u0, v0, boundaries);
+	SWAP(u0, u);
+	SWAP(v0, v);
+	advect(N, 1, u, u0, u0, v0, dt, boundaries);
+	advect(N, 2, v, v0, u0, v0, dt, boundaries);
+	project(N, u, v, u0, v0, boundaries);
 }
-
